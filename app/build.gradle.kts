3a8c1f57a21280762c5415945e3269cd4e164406
Almaz Kadyrov <43751325+ravexiii@users.noreply.github.com>
2026-04-30 04:44:25 +0600

    Bump Android versionCode to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "kg.freedge.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
